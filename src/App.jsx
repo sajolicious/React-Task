188ba916682cee25649358d7f03b1bf739e3e4bf
Sajol Das <sajoldas421@example.com>
2023-09-24 02:54:13 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,7 @@ function App() {
         <Route path="/" element={<Index />} />
         <Route path="/" element={<Menu />}>
           <Route path="problem-1" element={<Problem1 />} />
-          <Route path="/all-contacts" element={<Problem2 />} />
-          <Route path="/us-contacts" element={<Problem2 />} />
+         
           <Route path="/problem-2" element={<Problem2 />} />
          
         </Route>
